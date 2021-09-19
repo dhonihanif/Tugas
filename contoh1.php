4c8896406e58e0846374e69c31e8d68f06bb36d7
Dhoni Hanif Supriyadi <dhonihanif354@gmail.com>
2021-09-19 12:10:17 +0700

    Mengubah warna pada huruf perkenalkan

--- contoh1.php
+++ contoh1.php
@@ -3,7 +3,7 @@ class Contoh1 extends CI_Controller
 {
     public function index()
     {
-        echo "<h1 style='text-align:center;'>Perkenalkan</h1>";
+        echo "<h1 style='text-align:center;color:blue;'>Perkenalkan</h1>";
         echo "<p style='text-align:center;'>Nama Saya Dhoni Hanif Supriyadi<br>
             Saya tinggal di Lubang Buaya. <br>LOL
             adalah game kesukaan saya</p>";

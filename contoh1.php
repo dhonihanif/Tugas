dfc825b728aba7c450cd95e6022f8771946b28c9
Dhoni Hanif Supriyadi <dhonihanif354@gmail.com>
2021-09-19 12:08:07 +0700

    menambahkan index.html,contoh1.php

---
+++ contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1 style='text-align:center;'>Perkenalkan</h1>";
+        echo "<p style='text-align:center;'>Nama Saya Dhoni Hanif Supriyadi<br>
+            Saya tinggal di Lubang Buaya. <br>LOL
+            adalah game kesukaan saya</p>";
+    }
+}

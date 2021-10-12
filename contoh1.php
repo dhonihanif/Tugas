5b2833f9b48fc244fdbfc9c47e94d67d3d1dd579
Dhoni Hanif Supriyadi <dhonihanif354@gmail.com>
2021-10-12 16:13:41 +0700

    Pengupdatean

--- contoh1.php
+++ contoh1.php
@@ -3,8 +3,8 @@ class Contoh1 extends CI_Controller
 {
     public function index()
     {
-        echo "<h1 style='text-align:center;color:blue;'>Perkenalkan</h1>";
-        echo "<p style='text-align:center;'>Nama Saya Dhoni Hanif Supriyadi<br>
+        echo "<h1>Perkenalkan</h1>";
+        echo "<p style='text-align:left;'>Nama Saya Dhoni Hanif Supriyadi<br>
             Saya tinggal di Lubang Buaya. <br>LOL
             adalah game kesukaan saya</p><br />";
     }
